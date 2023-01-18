300b148fc1a64eace26e92371cd5c6475e9e2565
Roberto <rabustamante5@utpl.edu.ec>
2023-01-18 10:27:24 -0500

    avance

---
+++ Ejemplos/src/Paquete05/Operadortres.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete05;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Operadortres {
+    public static int obtenerMultiplicacion(int a,int b){
+        return a * b;
+    }
+}
+
--- Ejemplos/src/paquete03/Principal.java
+++ Ejemplos/src/paquete03/Principal.java
@@ -7,6 +7,8 @@ package paquete03;
 
 import paquete04.OperadorUno;
 import paquete04.OperadorDos;
+import Paquete05.Operadortres;
+//import paquete04.*;
 
 /**
  *
@@ -18,9 +20,12 @@ public class Principal {
         int valorA = OperadorUno.obtenerNumero(100);
         int valorB = OperadorUno.obtenerNumero(10);
         int suma = OperadorDos.obtenerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n",
-                valorA,
-                valorB,
-                suma);
+        int multiplicacion = Operadortres.obtenerMultiplicacion(valorA, valorB);
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %s\n ",
+                valorA, valorB,
+                suma,
+                valorA, valorB,
+                multiplicacion);
     }
 }
